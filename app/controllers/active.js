6953a09f880b61cb7c58b7986b1d7529ffb7df6a
Akshaya Senthil <akshaya.senthil@indexexchange.com>
2020-04-24 14:24:31 -0400

    add files

--- app/controllers/active.js
+++ app/controllers/active.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	todos: Ember.computed.filterBy('model', 'state', 'active')
-	// todos: Ember.computed.filter('model.@each.completed', m => m.completed === false)
+	// todos: Ember.computed.filter('model.@each.completed', m => m.completed === false && m.deferred == false)
 });
